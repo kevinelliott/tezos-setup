761464a07dc78b3707428026a31556308304434c
root <root@_HOSTNAME_>
2021-10-01 15:44:24 -0700

    Improvement

--- docker/tezos-node/import-snapshot.sh
+++ docker/tezos-node/import-snapshot.sh
@@ -6,20 +6,20 @@ TEZOS_HISTORY_MODE="${TEZOS_HISTORY_MODE:-rolling}"
 
 case $TEZOS_HISTORY_MODE in
   rolling)
-    TEZOS_SNAPSHOT=rolling
+    TEZOS_SNAPSHOT="rolling"
     ;;
   full)
-    TEZOS_SNAPSHOT=full
+    TEZOS_SNAPSHOT="full"
     ;;
   archive)
-    TEZOS_SNAPSHOT=full
+    TEZOS_SNAPSHOT="full"
     ;;
   *)
-    TEZOS_SNAPSHOT=full
+    TEZOS_SNAPSHOT="full"
     ;;
 esac
 
-echo "--- Tezos: Importing snapshot into Tezos Node path"
+echo "--- Tezos: Importing snapshot ($TEZOS_SNAPSHOT) into Tezos Node path"
 docker run --rm \
   -v ${TEZOS_HOST_PATH}/node:/var/run/tezos/node \
   -v ${TEZOS_HOST_PATH}/snapshots/${TEZOS_SNAPSHOT}:/snapshot \
