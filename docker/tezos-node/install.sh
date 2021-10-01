0affd0a225afca2f34d87055dd0edf04630256db
root <root@_HOSTNAME_>
2021-10-01 16:10:54 -0700

    Another small fix for snapshots

--- docker/tezos-node/install.sh
+++ docker/tezos-node/install.sh
@@ -7,8 +7,9 @@ source config.sh
 ./setup-env.sh
 
 if [ "${TEZOS_IMPORT_SNAPSHOT}" == "true" ]; then
-  ./get-snapshot.sh
-  ./import-snapshot.sh
+  TEZOS_SNAPSHOT_BLOCK="$(wget -qO- https://snaps.teztools.io/nodes/block)"
+  TEZOS_SNAPSHOT_BLOCK="${TEZOS_SNAPSHOT_BLOCK}" ./get-snapshot.sh
+  TEZOS_SNAPSHOT_BLOCK="${TEZOS_SNAPSHOT_BLOCK}" ./import-snapshot.sh
 fi
 
 TEZOS_VERSION="${TEZOS_VERSION}" TEZOS_HISTORY_MODE="${TEZOS_HISTORY_MODE}" docker-compose up -d
