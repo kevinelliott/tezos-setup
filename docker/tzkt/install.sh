7a8af019cbbf335a9ce882234888cef692fb3848
root <root@_HOSTNAME_>
2021-10-01 17:51:11 -0700

    Add TZKT

---
+++ docker/tzkt/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ../../config.sh
+
+ORIG_PATH=`pwd`
+
+rm -rf ${TEZOS_HOST_PATH}/tzkt
+git clone https://github.com/baking-bad/tzkt.git ${TEZOS_HOST_PATH}/tzkt
+
+echo "--- TZKT: Removing existing installation"
+cd ${TEZOS_HOST_PATH}/tzkt
+make stop
+make clean
+
+echo "--- TZKT: Intializing"
+make init
+
+echo "--- TZKT: Starting TZKT"
+make start
+
+cd $ORIG_PATH
